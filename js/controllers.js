e6ae94bb1d03e5688b188fc61bb640d906991cff
Justin <justin@cremalab.com>
2015-07-30 10:26:15 -0500

    Updating home

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,6 @@ var psychController = angular.module('psychController', []);
 
 psychController.controller('HomeController', ['$scope', function($scope) {
     $scope.view = 1;
-    console.log("this");
 
     this.setView = function(newView){
         $scope.view = newView;
